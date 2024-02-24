a1290a20843da243e51147092ea0edff86f55a23
gigaj0ule <j0ule@icloud.com>
2024-02-23 20:06:06 -0800

    whitespace fix

--- STM32CubeIDE_Code/Core/Inc/main.h
+++ STM32CubeIDE_Code/Core/Inc/main.h
@@ -85,51 +85,69 @@ void Error_Handler(void);
 #define Relay_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
-#define 		CONSTRAIN(x,lower,upper) ((x)<(lower)?(lower):((x)>(upper)?(upper):(x)))
-//##############################################
-#define 		PLL_Kp 					2.5e-5f 								// PID parameters for our PLL tracking control
-#define 		PLL_Ki 					5.0e-6f
-#define 		PLL_Kd 					0.0f
-#define 		PLL_LIMIT 				50.0f
-#define 		PLL_PERIOD 				3.125e-4f 								// 1 / (50 * 64) seconds
-#define 		F_CONVERSION_K 			-5.0e-2f
-//##############################################
-#define			I_OUT_Kp				20.0f									// PID parameters for our current output controller
-#define			I_OUT_Ki				10.0f
-#define			I_OUT_Kd				0.0f
-#define			I_OUT_Limit				400.0f									// Max voltage swing
-#define			I_OUT_PERIOD			75.0e-6f
-//##############################################
-#define			P_OUT_Kp				2.5e-2f									// PID parameters for our power output controller
-#define			P_OUT_Ki				2.5e-1f
-#define			P_OUT_Kd				0.0f
-#define			P_OUT_PERIOD			1.25e-3f
-#define			P_OUT_MIN				0.5										// Peak output current controller limits
-#define			P_OUT_MAX				4.0
-//##############################################
-#define 		SINE_STEP_PERIOD 		999 									// Ticks between incrementing our LO (Local Osc) index for 50Hz sine
-#define 		DUTY_LIMIT 				975 									// Our duty width can vary from -1000 to +1000
-#define			DUTY_MAX				1000.0f									// should be 999 but 1000 helps account for deadtime losses
-#define			INTEGRAL_SIZE			64
-#define			RMS_INTEGRAL_SIZE		64
-#define 		GRID_ACCEPTABLE 		1000
-#define 		GRID_OK					0
-#define 		GRID_UNACCEPTABLE 		-1000
-#define 		GRID_BAD_FAIL_RATE 		15 										// Some grid checks are allowed to fail for a certain amount of time (eg. Frequency) this parameter determines for how long
-#define			REQUEST_JOIN_GRID		2
-//##############################################
-#define 		RMS_LOWER_LIMIT 		50000 									// These values are for the grid checks. We disconnect if our metrics are out of these ranges (SI Units)
-#define 		RMS_UPPER_LIMIT 		70000									// Volts squared!
-#define 		FREQ_DEV_LIMIT 			0.5f
-#define			V_BUS_MINIMUM			360.0f
-#define 		V_BUS_MAXIMUM 			425.0f
-
-#define 		ENABLE_JOINING_GRID 	true
+#define    CONSTRAIN(x,lower,upper) ((x)<(lower)?(lower):((x)>(upper)?(upper):(x)))
 
 //##############################################
-#define 		I_GRID_SENSOR_K 		-5.65e-8
-#define 		V_BUS_SENSOR_K 			3.45e-6
-#define 		V_GRID_SENSOR_K 		3.45e-6
+// PID parameters for our PLL tracking control
+
+#define    PLL_Kp              2.5e-5f       
+#define    PLL_Ki              5.0e-6f
+#define    PLL_Kd              0.0f
+#define    PLL_LIMIT           50.0f
+#define    PLL_PERIOD          3.125e-4f    // 1 / (50 * 64) seconds
+#define    F_CONVERSION_K      -5.0e-2f
+
+//##############################################
+// PID parameters for our current output controller
+
+#define     I_OUT_Kp           20.0f                  
+#define     I_OUT_Ki           10.0f
+#define     I_OUT_Kd           0.0f
+#define     I_OUT_Limit        400.0f       // Max voltage swing
+#define     I_OUT_PERIOD       75.0e-6f
+
+//##############################################
+// PID parameters for our power output controller
+
+#define     P_OUT_Kp           2.5e-2f                  
+#define     P_OUT_Ki           2.5e-1f
+#define     P_OUT_Kd           0.0f
+#define     P_OUT_PERIOD       1.25e-3f
+#define     P_OUT_MIN          0.5          // Peak output current controller limits
+#define     P_OUT_MAX          4.0
+
+
+//##############################################
+// Timer Configs
+
+#define    SINE_STEP_PERIOD     999         // Ticks between incrementing our LO (Local Osc) index for 50Hz sine
+#define    DUTY_LIMIT           975         // Our duty width can vary from -1000 to +1000
+#define    DUTY_MAX             1000.0f     // should be 999 but 1000 helps account for deadtime losses
+#define    INTEGRAL_SIZE        64
+#define    RMS_INTEGRAL_SIZE    64
+#define    GRID_ACCEPTABLE      1000
+#define    GRID_OK              0
+#define    GRID_UNACCEPTABLE    -1000
+#define    GRID_BAD_FAIL_RATE   15          // Some grid checks are allowed to fail for a certain amount of time (eg. Frequency) this parameter determines for how long
+#define    REQUEST_JOIN_GRID    2
+
+//##############################################
+// Safe Operating Area
+
+#define    RMS_LOWER_LIMIT      50000       // These values are for the grid checks. We disconnect if our metrics are out of these ranges (SI Units)
+#define    RMS_UPPER_LIMIT      70000       // Volts squared!
+#define    FREQ_DEV_LIMIT       0.5f
+#define    V_BUS_MINIMUM        360.0f
+#define    V_BUS_MAXIMUM        425.0f
+
+#define    ENABLE_JOINING_GRID  true
+
+//##############################################
+// Sensor Constants
+
+#define    I_GRID_SENSOR_K     -5.65e-8
+#define    V_BUS_SENSOR_K       3.45e-6
+#define    V_GRID_SENSOR_K      3.45e-6
 
 /* USER CODE END Private defines */
 
